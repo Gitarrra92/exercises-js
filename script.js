fdb0814537c2d22532a9a6736b4e94face60f3f0
Natalia Pilewska <pilewska.natalia@gmail.com>
2018-01-14 18:56:04 +0100

    Changes

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-//Cokolwiek
+// var car = {
+//     color : "red",
+//     year : 2017,
+//     makeSound : function () {
+// console.log("wrrr")
+//     }
+// }
+//
+// car.makeSound()
+
+// function Car(color, year){
+//     this.color = color;
+//     this.year = year;
+//     this.makeSound = function() {
+//         console.log("wrrr")
+//     }
+// }
+//
+// var car = new Car("blue", 1990)
+//
+// console.log(car.makeSound())
+
+
+function Car(color, year){
+    this.color = color;
+    this.year = year;
+
+}
+Car.prototype.makeSound = function () {
+    console.log("wrrr")
+}
+
+var car = new Car("blue", 1990)
+ car.makeSound()
+
+
+
